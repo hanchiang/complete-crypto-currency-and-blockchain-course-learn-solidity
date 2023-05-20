e92c5ad65a5f3e6029b3d4875d589d5f07f989c9
han chiang <yaphc@hotmail.com>
2023-05-20 16:55:43 +0800

    Console game

--- 4-game-in-html/app/src/index.js
+++ 4-game-in-html/app/src/index.js
@@ -8,8 +8,11 @@ import threeInARowArtifact from "../../build/contracts/ThreeInARow.json"
 const App = {
   web3: null,
   account: null,
+  accountTwo: null,
+  activeAccount: null,
   gameManager: null,
   threeInARow: null,
+  activeThreeInARowInstance: null,
 
   start: async function() {
     const { web3 } = this;
@@ -24,6 +27,8 @@ const App = {
       // get accounts
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
+      this.activeAccount = this.account;
+      this.accountTwo = accounts[1];
 
       this.gameManager.defaults({
         from: this.account
@@ -31,15 +36,77 @@ const App = {
       this.threeInARow.defaults({
         from: this.account
       });
+      
     } catch (error) {
       console.error("Could not connect to contract or chain.", error);
     }
+  },
+  setAccountTwo: function() {
+    this.threeInARow.defaults({
+      from: this.accountTwo
+    });
+    this.activeAccount = this.accountTwo;
   },
   refreshHighscore: async function() {
     const { gameManager } = this;
     const gameManagerInstance = await gameManager.deployed();
     const top10 = await gameManagerInstance.getTop10();
     console.log(top10);
+  },
+  createNewGame: async function() {
+    const { gameManager, web3, threeInARow }  = this;
+
+    const gameManagerInstance = await gameManager.deployed();
+    const txResult = await gameManagerInstance.startNewGame({ value: web3.utils.toWei('0.1', 'ether') });
+    console.log(txResult);
+    this.activeThreeInARowInstance = await threeInARow.at(txResult.logs[0].args._game);
+
+    this.activeThreeInARowInstance.PlayerJoined().on('data', (event) => {
+      console.log(`player joined event:`, event);
+      event.args._player;
+    })
+
+    this.listenToEvents();
+  },
+  joinGame: async function(gameAddress) {
+    const { web3, threeInARow } = this;
+
+    this.activeThreeInARowInstance = await threeInARow.at(gameAddress);
+
+    this.listenToEvents();
+
+    await this.activeThreeInARowInstance.joinGame({ from: this.accountTwo, value: web3.utils.toWei('0.1', 'ether') });
+  },
+  setStone: async function(row, col) {
+    await this.activeThreeInARowInstance.setStone(row, col);
+  },
+  updateBoard: async function() {
+    const board = await this.activeThreeInARowInstance.getBoard();
+    console.log(board);
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board[r].length; c++) {
+        if (board[r][c] == this.activeAccount) {
+          $("#board")[0].children[0].children[r].children[c].innerHTML = "X";
+        } else if (board[r][c] != 0) {
+          $("#board")[0].children[0].children[r].children[c].innerHTML = "O";
+        }
+      }
+    }
+  },
+  listenToEvents: async function() {
+    const { activeThreeInARowInstance, web3 }  = this;
+    const self = this;
+
+    activeThreeInARowInstance.NextPlayer().on('data', (event) => {
+      console.log(`next player event:`, event);
+      if (event.args._player == self.activeAccount) {
+        console.log("Your turn!");
+      } else {
+        console.log("Waiting for opponent");
+      }
+      self.updateBoard();
+    })
   },
   setStatus: function(message) {
     const status = document.getElementById("status");
