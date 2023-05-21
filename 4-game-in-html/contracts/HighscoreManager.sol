196f040757536399b3ee15ff6c14858a8b16a3f3
han chiang <yaphc@hotmail.com>
2023-05-21 10:20:32 +0800

    Update HighscoreManager and test

--- 4-game-in-html/contracts/HighscoreManager.sol
+++ 4-game-in-html/contracts/HighscoreManager.sol
@@ -14,6 +14,7 @@ contract HighscoreManager {
     }
 
     mapping(address => Highscore) public highscoreMapping;
+    uint8 numAddressesInHighscore;
     address public highscoreHolder;
     
     constructor(address _gameManager) public {
@@ -35,8 +36,9 @@ contract HighscoreManager {
         // update highscore holder
         if (highscoreHolder == address(0x0)) {
             highscoreHolder = _winner;
-            return;
+            numAddressesInHighscore++;
         }
+
         if (highscoreHolder == _winner) {
             return;
         }
@@ -44,13 +46,17 @@ contract HighscoreManager {
         // update winner list
         address currentPosition = highscoreHolder;
         for (uint i = 0; i < NUM_HIGH_SCORES; i++) {
-            // not yet added to the highscore list
+            // winner is not yet added to the highscore list
             // TODO: Maintain a tail pointer for convenience
-            if (i < NUM_HIGH_SCORES - 1 && highscoreMapping[currentPosition].next == address(0) &&
+            bool winnerAlreadyInList = highscoreMapping[_winner].numberOfWins > 1;
+
+            if (numAddressesInHighscore < NUM_HIGH_SCORES && !winnerAlreadyInList &&
+                highscoreMapping[currentPosition].next == address(0) &&
                 highscoreMapping[_winner].prev == address(0) && highscoreMapping[_winner].next == address(0)
             ) {
                 highscoreMapping[currentPosition].next = _winner;
                 highscoreMapping[_winner].prev = currentPosition;
+                numAddressesInHighscore++;
                 return;
             }
             if (highscoreMapping[currentPosition].numberOfWins < highscoreMapping[_winner].numberOfWins) {
@@ -63,20 +69,35 @@ contract HighscoreManager {
                 // winner is the last position, and move up a few positions
                 // before 1(10)-2(6)-3(6)-4(6), after: 1(10)-4(7)-2(6)-3(6)
 
-                // connect current's prev to winner
-                if (highscoreMapping[currentPosition].prev != address(0x0)) {
-                    highscoreMapping[highscoreMapping[currentPosition].prev].next = _winner;
-                }
-                // connect winner's prev to winner's next
+                // update winner's prev
                 if (highscoreMapping[_winner].prev != address(0x0)) {
                     highscoreMapping[highscoreMapping[_winner].prev].next = highscoreMapping[_winner].next;
                 }
-                // update winner connection
+                // update winner's next
+                if (highscoreMapping[_winner].next != address(0x0)) {
+                    highscoreMapping[highscoreMapping[_winner].next].prev = highscoreMapping[_winner].prev;
+                }
+                // update current's prev
+                if (highscoreMapping[currentPosition].prev != address(0x0)) {
+                    highscoreMapping[highscoreMapping[currentPosition].prev].next = _winner;
+                }
+                // update current's next
+                if (highscoreMapping[currentPosition].next != address(0x0)) {
+                    highscoreMapping[highscoreMapping[currentPosition].next].prev = highscoreMapping[currentPosition].prev;
+                }
+                
+                // update winner
                 highscoreMapping[_winner].prev = highscoreMapping[currentPosition].prev;
                 highscoreMapping[_winner].next = currentPosition;
 
-                // update current connection
+                // update current
                 highscoreMapping[currentPosition].prev = _winner;
+
+                // last place is out of the list
+                if (i == NUM_HIGH_SCORES - 1) {
+                    highscoreMapping[currentPosition].prev = address(0);
+                    highscoreMapping[currentPosition].next = address(0);
+                }
 
                 if (highscoreMapping[_winner].numberOfWins > highscoreMapping[highscoreHolder].numberOfWins) {
                     highscoreHolder = _winner;
@@ -84,8 +105,10 @@ contract HighscoreManager {
                 return;
             }
             currentPosition = highscoreMapping[currentPosition].next;
+            if (currentPosition == address(0)) {
+                return;
+            }
         }
-        
     }
 
     function getTop10() public view returns(address[10] memory, uint[10] memory){
--- 4-game-in-html/test/TestHighscoreManager.sol
+++ 4-game-in-html/test/TestHighscoreManager.sol
@@ -23,7 +23,7 @@ contract TestHighscoreManager {
         uint[10] memory top10Wins;
         (top10Addresses, top10Wins) = highscoreManager.getTop10();
         for (uint i = 0; i < winners.length; i++) {
-            Assert.equal(top10Addresses[i], winners[i], "Should have 1 winner");
+            Assert.equal(top10Addresses[i], winners[i], "Expect winner");
             Assert.equal(top10Wins[i], 1, "Winner should have score 1");
         }
     }
@@ -71,8 +71,8 @@ contract TestHighscoreManager {
         highscoreManager.addWinner(winners[winners.length - 1]);
 
         address[10] memory expectedWinners = [
-            address(10), address(1), address(2), address(3), address(4), address(5),
-            address(6), address(7), address(8), address(9)
+            address(10), address(1), address(2), address(3), address(4),
+            address(5), address(6), address(7), address(8), address(9)
         ];
         uint256[10] memory expectedWins = [
             uint256(2), uint256(1), uint256(1), uint256(1), uint256(1),
@@ -107,6 +107,69 @@ contract TestHighscoreManager {
         uint256[10] memory expectedWins = [
             uint256(2), uint256(1), uint256(1), uint256(1), uint256(1),
             uint256(1), uint256(1), uint256(1), uint256(1), uint256(1)
+        ];
+
+        address[10] memory top10Addresses;
+        uint[10] memory top10Wins;
+        (top10Addresses, top10Wins) = highscoreManager.getTop10();
+        for (uint i = 0; i < winners.length; i++) {
+            Assert.equal(top10Addresses[i], expectedWinners[i], "Expect winner");
+            Assert.equal(top10Wins[i], expectedWins[i], "Expect win");
+        }
+    }
+
+    function testWinnerNotInTheList_noChangeInLastPlace() public {
+        HighscoreManager highscoreManager = new HighscoreManager(address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4));
+
+        address[10] memory winners = [
+            address(1), address(2), address(3), address(4), address(5),
+            address(6), address(7), address(8), address(9), address(10)
+        ];
+        for (uint i = 0; i < winners.length; i++) {
+            highscoreManager.addWinner(winners[i]);
+        }
+        highscoreManager.addWinner(address(11));
+
+        address[10] memory expectedWinners = [
+            address(1), address(2), address(3), address(4), address(5),
+            address(6), address(7), address(8), address(9), address(10)
+        ];
+        uint256[10] memory expectedWins = [
+            uint256(1), uint256(1), uint256(1), uint256(1), uint256(1),
+            uint256(1), uint256(1), uint256(1), uint256(1), uint256(1)
+        ];
+
+        address[10] memory top10Addresses;
+        uint[10] memory top10Wins;
+        (top10Addresses, top10Wins) = highscoreManager.getTop10();
+        for (uint i = 0; i < winners.length; i++) {
+            Assert.equal(top10Addresses[i], expectedWinners[i], "Expect winner");
+            Assert.equal(top10Wins[i], expectedWins[i], "Expect win");
+        }
+    }
+
+    function testWinnerNotInTheList_changeInLastPlace() public {
+        HighscoreManager highscoreManager = new HighscoreManager(address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4));
+
+        address[9] memory winners = [
+            address(1), address(2), address(3), address(4), address(5),
+            address(6), address(7), address(8), address(9)
+        ];
+        for (uint i = 0; i < winners.length; i++) {
+            highscoreManager.addWinner(winners[i]);
+            highscoreManager.addWinner(winners[i]);
+        }
+        highscoreManager.addWinner(address(10));
+        highscoreManager.addWinner(address(11));
+        highscoreManager.addWinner(address(11));
+
+        address[10] memory expectedWinners = [
+            address(1), address(2), address(3), address(4), address(5),
+            address(6), address(7), address(8), address(9), address(11)
+        ];
+        uint256[10] memory expectedWins = [
+            uint256(2), uint256(2), uint256(2), uint256(2), uint256(2),
+            uint256(2), uint256(2), uint256(2), uint256(2), uint256(2)
         ];
 
         address[10] memory top10Addresses;
